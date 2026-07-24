f1b486898034ee296584e4ca91156a952ae36349
kionDepZaiNhatQuaDat <kienheo05092007@gmail.com>
2026-07-24 10:08:05 +0700

    Clean initial commit for Unity

---
+++ Assets/MapTrasition.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Runtime.CompilerServices;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class MapTrasition : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] PolygonCollider2D mapBoundary;
+    CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos = 2;
+
+    enum Direction {Up, Down, Left, Right}
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
+            confiner.InvalidateBoundingShapeCache();
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x += additivePos;
+                break;
+            case Direction.Right:
+                newPos.x -= additivePos;
+                break;
+        }
+        player.transform.position = newPos;
+    }
+}
